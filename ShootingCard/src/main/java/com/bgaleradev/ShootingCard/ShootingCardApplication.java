f4c2a57b442abddbfdd0efeea8ca7483b89cd87f
bgaleradev <104566801+bgaleradev@users.noreply.github.com>
2022-10-28 08:47:07 +0200

    Add files via upload
    Initial state of the application with the main dependencies in the pom file

---
+++ ShootingCard/src/main/java/com/bgaleradev/ShootingCard/ShootingCardApplication.java
@@ -0,0 +1,13 @@
+package com.bgaleradev.ShootingCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootingCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootingCardApplication.class, args);
+	}
+
+}
---
+++ ShootingCard/src/test/java/com/bgaleradev/ShootingCard/ShootingCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgaleradev.ShootingCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootingCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
